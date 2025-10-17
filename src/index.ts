077963c6ef8809e0d5a76c949cb946b4214707f6
shaunakc11 <helloshaunak276@gmail.com>
2025-10-17 09:49:52 +0530

    update CORS configuration to allow requests from any origin with credentials

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
     credentials: true,
   })
 );
