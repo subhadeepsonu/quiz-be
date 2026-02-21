21abeb9b58e792d47a573f5f264716ddca0053fd
shaunakc11 <helloshaunak276@gmail.com>
2026-02-21 15:12:13 +0530

    redep

--- src/db.ts
+++ src/db.ts
@@ -5,3 +5,4 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
